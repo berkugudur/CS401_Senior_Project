50de9bb15426048233500fff54172615635d4000
berkugudur <berk.ugudur@ozu.edu.tr>
2018-10-14 21:07:28 +0300

    json parse added

--- analyzer.py
+++ analyzer.py
@@ -1,6 +1,8 @@
+import json
 import sys
 
 expected_args = ["file", "source-file", "target-file"]
+printable_values = ["action", "state", "hp", "x", "y"]
 
 if len(sys.argv) != len(expected_args):
     print "ERROR:",
@@ -9,3 +11,31 @@ if len(sys.argv) != len(expected_args):
     sys.exit(0)
 
 args = dict(zip(expected_args, sys.argv))
+
+
+# def functions
+
+def print_player_values(player_data):
+    line = ""
+    for value in printable_values:
+        line += str(player_data[value]) + ","
+    return line[:-1]
+
+
+def print_frame(frame_data):
+    print str(frame_data["current_frame"]) + "," + print_player_values(frame_data["P1"]) + "," + print_player_values(
+        frame_data["P2"])
+
+
+def print_round(round_data):
+    for frame_data in round_data:
+        print_frame(frame_data)
+
+
+# end of functions
+
+with open(args["source-file"], "r") as source:
+    data_ = json.load(source)
+
+for round_data in data_["rounds"]:
+    print_round(round_data)
