284e0f5ddd35ef96e942db7245d061ffe58fbb42
berkugudur <berk.ugudur@ozu.edu.tr>
2018-10-17 03:50:51 +0300

    action types map added

--- analyzer.py
+++ analyzer.py
@@ -17,12 +17,14 @@ if len(sys.argv) < len(expected_args):
 args = dict(zip(expected_args, sys.argv))
 x_as_category = True
 
+
 # def functions
 def convert_dif_x_to_category(x):
     if x <= 0:
         x = -x
     x = x / 50
     return x
+
 
 def print_player_values(player_data):
     line = ""
@@ -59,19 +61,31 @@ def write_round_standb(round_data, writer):
         if get_player_values(frame_data["P1"])[0] == "STAND_B" or get_player_values(frame_data["P1"])[0] == "STAND_FB":
             if action != last_action:
                 if x_as_category:
-                    x_category = convert_dif_x_to_category(get_player_values(frame_data["P1"])[3] - get_player_values(frame_data["P2"])[3])
+                    x_category = convert_dif_x_to_category(
+                        get_player_values(frame_data["P1"])[3] - get_player_values(frame_data["P2"])[3])
                 else:
                     x_category = get_player_values(frame_data["P1"])[3] - get_player_values(frame_data["P2"])[3]
-                writer.writerow([frame_data["current_frame"]] + [get_player_values(frame_data["P1"])[0]] + [get_player_values(frame_data["P2"])[0]] + [x_category] )
+                writer.writerow([frame_data["current_frame"]] + [get_player_values(frame_data["P1"])[0]] + [
+                    get_player_values(frame_data["P2"])[0]] + [x_category])
         if args["print"] == "1":
             print_frame(frame_data)
         last_data = frame_data
+
+
+def get_action_type_map(reader):
+    type_map = dict()
+    for row in reader:
+        type_map.setdefault(row[1], [])
+        type_map[row[1]].append(row[0])
+    return type_map
 
 
 # end of functions
 
 data_ = json.load(open(args["source-file"], "r"))
 writer_ = csv.writer(open(args["target-file"], "w"), delimiter=',')
+reader_ = csv.reader(open("data/ActionTypes.csv"))
 
+action_type_map = get_action_type_map(reader_)
 for round_data in data_["rounds"]:
-    write_round_standb(round_data, writer_)
+    write_round(round_data, writer_)
