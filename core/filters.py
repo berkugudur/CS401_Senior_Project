7d0980bf90ade56d20dcff26c785211da7fbca5c
berkugudur <berk.ugudur@ozu.edu.tr>
2018-11-10 19:52:20 +0300

    Added remove unpressed frames filter

--- core/filters.py
+++ core/filters.py
@@ -2,6 +2,28 @@ from core.actions import ACTION_MAP
 
 # These are filter methods. They are responsible for removing unnecessary frames from game data.
 
+def remove_unpressed_frames(colums, frames):
+    # Check if player pressed any key
+    def is_pressed(frame_data):
+        key_a = frame_data[colums.index("P2-key_a")]
+        key_b = frame_data[colums.index("P2-key_b")]
+        key_c = frame_data[colums.index("P2-key_c")]
+        key_u = frame_data[colums.index("P2-key_up")]
+        key_d = frame_data[colums.index("P2-key_down")]
+        key_l = frame_data[colums.index("P2-key_left")]
+        key_r = frame_data[colums.index("P2-key_right")]
+        return key_a or key_b or key_c or key_u or key_d or key_l or key_r
+
+    # Remove frames that unpressed by player
+    def init(round_data):
+        prepared_frames = []
+        for frame_data in round_data:
+            if is_pressed(frame_data):
+                prepared_frames.append(frame_data)
+        return prepared_frames
+
+    return init(frames)
+
 def remove_both_standing_frames(columns, frames):
     # Check is both players are standing
     def is_both_players_stand(frame_data):
@@ -14,7 +36,7 @@ def remove_both_standing_frames(columns, frames):
         for frame_data in round_data:
             if not is_both_players_stand(frame_data):
                 prepared_frames.append(frame_data)
-        return prepared_frames;
+        return prepared_frames
     
     return init(frames)
 
@@ -46,7 +68,6 @@ def remove_recov_frames(columns, frames):
         for frame_data in round_data:
             if not is_recov_frame(frame_data):
                 prepared_frames.append(frame_data)
-            prev_frame = frame_data
         return prepared_frames
     
     return init(frames)
