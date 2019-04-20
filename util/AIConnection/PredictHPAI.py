4f6e89531abc7cbc95cd0154b2cf417e320d4bce
Meriç Öztiryaki <oztiryakimeric@gmail.com>
2019-04-20 22:12:51 +0300

    Refactor PredictHPAI
    From now on it can capable of running "n" different models

--- util/AIConnection/PredictHPAI.py
+++ util/AIConnection/PredictHPAI.py
@@ -4,8 +4,16 @@ from BotPredictor import BotPredictor
 
 class PredictHPAI(object):
     def __init__(self, gateway):
-        self.BCPvsThunderBot = BotPredictor("example_data/bcpvsthunder/hpmodel/", "example_data/bcpvsthunder/actionmodel/")
-        self.BCPvsUtalBot = BotPredictor("example_data/bcpvsutal/hpmodel/", "example_data/bcpvsutal/actionmodel/")
+        print("hp ai created")
+        self.predictors = [
+            BotPredictor("example_data/bcpvsthunder/hpmodel/", "example_data/bcpvsthunder/actionmodel/"),
+            BotPredictor("example_data/bcpvsutal/hpmodel/", "example_data/bcpvsutal/actionmodel/")
+        ]
+        # For help logging
+        self.predictor_names = [
+            "Thunder",
+            "Utal"
+        ]
         self.gateway = gateway
         
     def close(self):
@@ -32,6 +40,10 @@ class PredictHPAI(object):
     def input(self):
         return self.inputKey
     
+    # Return true if given points are close to do attack actions.
+    def are_they_close(self, us_x, us_y, opponent_x, opponent_y):
+        return True    
+
     def processing(self):
         if self.frameData.getEmptyFlag() or self.frameData.getRemainingTime() <= 0:
             self.isGameJustStarted = True
@@ -57,19 +69,35 @@ class PredictHPAI(object):
             return
         self.inputKey.empty()
         self.cc.skillCancel()
-
-        bcpvsthunder_predicted_hp = self.BCPvsThunderBot.get_predicted_hp(action, opponent_x, opponent_y, us_x, us_y)
-        bcpvsutal_predicted_hp = self.BCPvsUtalBot.get_predicted_hp(action, opponent_x, opponent_y, us_x, us_y)
-        print ( "thunder: " + str(int(bcpvsthunder_predicted_hp)))
-        print ( "utal: " + str(int(bcpvsutal_predicted_hp)))
-        print ( "----------------")
         
-        if bcpvsthunder_predicted_hp >= bcpvsutal_predicted_hp:
-            predicted_action = self.BCPvsThunderBot.get_predicted_action(action, opponent_hp, opponent_x, opponent_y, us_hp, us_x, us_y)
-        else:
-            predicted_action = self.BCPvsUtalBot.get_predicted_action(action, opponent_hp, opponent_x, opponent_y, us_hp, us_x, us_y)
+        # If they are not close hp prediction mechanism will fail therefore use just action prediction.
+        if self.are_they_close(us_x, us_y, opponent_x, opponent_y):
+            predicted_hps = []
 
-        self.cc.commandCall(predicted_action)
+            # Evaluate all predictions for each predictor
+            for index, predictor in enumerate(self.predictors):
+                predicted_hp = predictor.get_predicted_hp(action, opponent_x, opponent_y, us_x, us_y)
+                predicted_hps.append(predicted_hp)
+
+                print(self.predictor_names[index] + ": " + str(predicted_hp))
+
+            # Find maximum valued predictor 
+            maximum_hp = max(predicted_hps)
+            index_of_predictor = predicted_hps.index(maximum_hp)
+            maximum_predictor = self.predictors[index_of_predictor]
+
+            print("\n\t" + self.predictor_names[index_of_predictor] + " has the biggest hp value.\n")
+            print("---------------------------------------")
+
+            predicted_action = maximum_predictor.get_predicted_action(action, opponent_hp, opponent_x, opponent_y, us_hp, us_x, us_y)
+
+            self.cc.commandCall(predicted_action)
+        else: # They are not close, just apply the first bot's predicted action.
+            print("They are not close to predict hp action.")
+            predicted_action = self.predictors[0].get_predicted_action(action, opponent_hp, opponent_x, opponent_y, us_hp, us_x, us_y)
+            self.cc.commandCall(predicted_action)
+
+
                         
     class Java:
         implements = ["aiinterface.AIInterface"]
